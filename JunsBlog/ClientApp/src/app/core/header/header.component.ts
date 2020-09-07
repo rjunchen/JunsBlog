7e5ca4beaa148579fa267215f7e950f6cd852580
rjunchen <rjunchen@gmail.com>
2020-09-07 11:15:47 -0700

    add home and back buttons

--- JunsBlog/ClientApp/src/app/core/header/header.component.ts
+++ JunsBlog/ClientApp/src/app/core/header/header.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { EventService } from '../../services/event.service'
 import { AuthenticationService } from 'src/app/services/authentication.service';
 import { User } from 'src/app/models/authentication/user';
+import { Location } from '@angular/common'
 
 @Component({
   selector: 'app-header',
@@ -12,7 +13,7 @@ export class HeaderComponent implements OnInit {
 
   currentUser: User;
 
-  constructor(public auth: AuthenticationService, private eventService: EventService) { 
+  constructor(public auth: AuthenticationService, private eventService: EventService, private location: Location) { 
     this.auth.onUserInfoUpdated.subscribe(data => this.currentUser = data);
   }
 
@@ -22,5 +23,9 @@ export class HeaderComponent implements OnInit {
 
   toggleSidebar(){
     this.eventService.toggleSidebar();
+  }
+
+  goBack(){
+    this.location.back();
   }
 }
